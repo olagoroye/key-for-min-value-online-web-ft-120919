21c0f6208e328ee484dd1a8a009bc35243068a87
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-13 23:58:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+# def key_for_min_value(name_hash)
 # if name_hash = {}
 #     return nil
 #   else
@@ -16,6 +16,13 @@ def key_for_min_value(name_hash)
 #       min_key
 
 # end
+
+
+def key_for_min_value(name_hash)
+  lowest_value = 0
+  lowest_key = nil
+  
+  name_hash.each do |key,value|
 end
 
 
