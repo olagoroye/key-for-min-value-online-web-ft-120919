a718cf487334ec910a71020fada27dea449997b9
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 00:31:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -43,5 +43,14 @@ def key_for_min_value(name_hash)
       current_minimum_val = value 
       current_key_assoc_w_min_val = key 
     else 
+      if current_minimum_val > value 
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key 
+      end
+    end
+  end 
+  current_key_assoc_w_min_val 
+end 	end 
+
 
 
