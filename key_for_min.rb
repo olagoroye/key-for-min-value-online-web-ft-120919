05704ed29ccc0884e252739e58cb18317caa5635
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 00:22:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,8 +19,8 @@
 
 def key_for_min_value(name_hash)
 if name_hash.length == {}
-  min_value = name_hash.first[1]
-      min_key = name_hash.first[0]
+  min_value = name_hash.length[0]
+      min_key = name_hash.length[0]
       name_hash.each do |key, value|
         if value > min_value
           min_value = value
